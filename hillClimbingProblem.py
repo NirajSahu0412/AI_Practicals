e625ad1e38be006c1e8fe3b88cceb065a19af1b3
Shanukumar Singh <91169674+Shanu2409@users.noreply.github.com>
2023-09-12 23:08:27 +0530

    Update hillClimbingProblem.py

--- hillClimbingProblem.py
+++ hillClimbingProblem.py
@@ -1,22 +1,22 @@
 import random
 
 def objectiveFunction(x):
-    return -(x - 3 * x - 5 * x - 7 + 17)
+    return -(x - 15 * x + 10)
 
-def hillClimbing(maxIteration = 1000, stepSize = 0.1):
-    currentSolution = random.uniform(0,6)
+def hillClimbing(maxIteration=1000, stepSize=0.1):
+    currentSolution = random.uniform(0, 6)
     currentValue = objectiveFunction(currentSolution)
+
     for _ in range(maxIteration):
         newSolution = currentSolution + random.uniform(-stepSize, stepSize)
-        if(0 <= newSolution <= 6):
+        if 0 <= newSolution <= 6:
             newValue = objectiveFunction(newSolution)
-            if(newValue > currentValue):
-                currentValue = newValue
+            if newValue > currentValue:
+                currentSolution, currentValue = newSolution, newValue
+
     return currentSolution, currentValue
 
-if(__name__ == "__main__"):
-    maxIteration = 1000
-    stepSize = 0.1
-    bestSolution, bestValue = hillClimbing(maxIteration, stepSize)
-    print(bestSolution)
-    print(bestValue)
+
+bestSolution, bestValue = hillClimbing()
+print(bestSolution)
+print(bestValue)
