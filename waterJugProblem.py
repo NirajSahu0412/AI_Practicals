09286404c122c196cb593f00006d531f1eb9589a
Shanukumar Singh <91169674+Shanu2409@users.noreply.github.com>
2023-09-12 23:11:39 +0530

    Update waterJugProblem.py

--- waterJugProblem.py
+++ waterJugProblem.py
@@ -1,87 +1,39 @@
 capacity = (8, 5, 3)
-x = capacity[0]
-y = capacity[1]
-z = capacity[2]
-
-memory = {}
-
+memory = set()
 ans = []
 
-def getAllStates(state):
-    a = state[0]
-    b = state[1]
-    c = state[2]
+def get_next_state(state, from_jug, to_jug):
+    max_from, max_to, _ = capacity
+    if state[from_jug] > 0 and state[to_jug] < max_to:
+        amount_to_move = min(state[from_jug], max_to - state[to_jug])
+        new_state = list(state)
+        new_state[from_jug] -= amount_to_move
+        new_state[to_jug] += amount_to_move
+        return tuple(new_state)
+    return None
 
-    if(a == 4 and b == 4):
+def getAllStates(state):
+    if state[0] == 4 and state[1] == 4:
         ans.append(state)
         return True
     
-    if((a, b, c) in memory):
+    if state in memory:
         return False
     
-    memory[(a, b, c)] = 1
+    memory.add(state)
 
-    if(a > 0):
-        if(a + b <= y):
-            if(getAllStates((0, a + b, c))):
-                ans.append(state)
-                return True            
-        else:
-            if(getAllStates((a - (y - b), y, c))):
-                ans.append(state)
-                return True
-            
-        if(a + c <= z):
-            if(getAllStates((0, b, a + c))):
-                ans.append(state)
-                return True
-        else:
-            if(getAllStates((a - (z - c), b, z))):
-                ans.append(state)
-                return True
-            
-    if(b > 0):
-        if(a + b <= x):
-            if(getAllStates((a + b, 0, c))):
-                ans.append(state)
-                return True            
-        else:
-            if(getAllStates((x, b - (x - a), c))):
-                ans.append(state)
-                return True
-            
-        if(b + c <= z):
-            if(getAllStates((a, 0, b + c))):
-                ans.append(state)
-                return True
-        else:
-            if(getAllStates((a, b - (z - c), z))):
-                ans.append(state)
-                return True
-            
-    if(c > 0):
-        if(a + c <= x):
-            if(getAllStates((a + c, b, 0))):
-                ans.append(state)
-                return True            
-        else:
-            if(getAllStates((x, b, c- (x - a)))):
-                ans.append(state)
-                return True
-            
-        if(b + c <= y):
-            if(getAllStates((a, b + c, 0))):
-                ans.append(state)
-                return True
-        else:
-            if(getAllStates((a, y, c - (y - b)))):
-                ans.append(state)
-                return True
-            
+    for from_jug in range(3):
+        for to_jug in range(3):
+            if from_jug != to_jug:
+                new_state = get_next_state(state, from_jug, to_jug)
+                if new_state and getAllStates(new_state):
+                    ans.append(state)
+                    return True
+
     return False
 
 initialState = (8, 0, 0)
-print("Starting Work.....\n")
+print("Starting Work...\n")
 getAllStates(initialState)
 ans.reverse()
 for i in ans:
